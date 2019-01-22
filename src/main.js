5d9dcb59b05935b2c56ed76b74e6ac1f89219eb4
N.TAHAR <najibtaharberrabah@gmail.com>
2019-01-22 09:09:50 +0100

    script library changed

---
+++ src/main.js
@@ -0,0 +1,36 @@
+
+// let title = $(document.createElement("h1")).addClass('title');
+// let infoProject = $(document.createElement("p")).addClass('info-project');
+// $('.info-container').append(title);
+
+$(function(){
+  $.scrollify({
+    section : ".firstSection",
+    interstitialSection : ".firstSection, .secondSection",
+    scrollSpeed: 1000,
+    before: function(section){
+      if(section===0){
+        $('.content').addClass('disappear-content');
+        $('.firstSection').addClass('active');
+        $('.secondSection').removeClass('active');
+        $('.view').addClass('reset');
+        setTimeout(function(){ 
+          $('.view').removeClass('reset');
+          $('.title').html('Wanted');
+          $('.content').removeClass('disappear-content');
+        }, 500);
+      }
+      if(section===1){
+        $('.content').addClass('disappear-content');
+        $('.secondSection').addClass('active');
+        $('.firstSection').removeClass('active');
+        $('.view').addClass('reset');
+        setTimeout(function(){ 
+          $('.view').removeClass('reset');
+          $('.title').html('Bye');
+          $('.content').removeClass('disappear-content');
+        }, 500);
+      }
+      }
+  });
+});
